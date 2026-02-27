11b90c393a8ab71e27f4657402208cf4402a9606
Luis Devia <luisangeldevia679@gmail.com>
2026-02-27 14:53:37 -0500

    Primera opcion

--- cajero.py
+++ cajero.py
@@ -1,3 +1,8 @@
 print("Bienvenidos al cajero automático")
 saldo = 1000
 n_de_operaciones = int(input("por favor ingrese cuantas operaciones quiere realizar: "))
+
+ #opcion 1
+if opcion == "1":
+         print (saldo)
+         
