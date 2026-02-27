0e2adda8332639ce70aa5bee13c513b3bfdcc059
Luis Devia <luisangeldevia679@gmail.com>
2026-02-27 15:02:55 -0500

    Arreglo de op 1 e inserto de op 2

--- cajero.py
+++ cajero.py
@@ -2,7 +2,26 @@ print("Bienvenidos al cajero automático")
 saldo = 1000
 n_de_operaciones = int(input("por favor ingrese cuantas operaciones quiere realizar: "))
 
+         
+#menu
+for i in range(n_de_operaciones):
+    print ("1 → Consultar saldo")
+    print ("2 → Retirar dinero")
+    print ("3 → Depositar dinero")
+    opcion = input("por favor ingrese la opcion: ")
+    
  #opcion 1
 if opcion == "1":
          print (saldo)
-         
+
+elif opcion == "2":
+        #opcion 2
+        retirar = int(input("por favor ingrese cuanto desea retirar: "))
+        while retirar < 0:
+            retirar = float(input("es un numero negativo, ingresa una cantidad valida"))
+        
+        if retirar > saldo:
+            print("monto insuficiente")
+        elif retirar <= saldo: 
+            saldo -= retirar
+            print (f"saldo actual: {saldo} ")
